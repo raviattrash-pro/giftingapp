c4b949c656d784150b6815785988e83d4e901b7e
raviattrash-pro <raviattrash1@gmail.com>
2026-06-28 01:50:12 +0530

    fix: use JSON serialization for Redis cache to prevent 500 errors

--- backend/src/main/java/com/giftconcierge/config/CacheConfig.java
+++ backend/src/main/java/com/giftconcierge/config/CacheConfig.java
@@ -16,19 +16,19 @@ import java.time.Duration;
 @EnableCaching
 public class CacheConfig {
 
-//    @Bean
-//    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-//        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
-//                .entryTtl(Duration.ofHours(1))
-//                .serializeKeysWith(SerializationPair.fromSerializer(new StringRedisSerializer()))
-//                .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
-//                .disableCachingNullValues();
-//
-//        return RedisCacheManager.builder(connectionFactory)
-//                .cacheDefaults(defaultConfig)
-//                .withCacheConfiguration("gifts", defaultConfig.entryTtl(Duration.ofHours(2)))
-//                .withCacheConfiguration("users", defaultConfig.entryTtl(Duration.ofMinutes(30)))
-//                .withCacheConfiguration("recipients", defaultConfig.entryTtl(Duration.ofMinutes(30)))
-//                .build();
-//    }
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1))
+                .serializeKeysWith(SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultConfig)
+                .withCacheConfiguration("gifts", defaultConfig.entryTtl(Duration.ofHours(2)))
+                .withCacheConfiguration("users", defaultConfig.entryTtl(Duration.ofMinutes(30)))
+                .withCacheConfiguration("recipients", defaultConfig.entryTtl(Duration.ofMinutes(30)))
+                .build();
+    }
 }
